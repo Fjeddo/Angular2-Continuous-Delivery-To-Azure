163e1f099526545d76b376598ad28a1fcbae40b2
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2016-09-19 11:30:43 +0200

    The angular2 app in place

--- NodejsWebAppApplication/gulpfile.js
+++ NodejsWebAppApplication/gulpfile.js
@@ -4,7 +4,14 @@ var ts = require('gulp-typescript');
 gulp.task('default', function() {
     return gulp.src('scripts/**/*.ts')
         .pipe(ts({
-            noImplicitAny: true
+            "target": "es5",
+            "module": "commonjs",
+            "moduleResolution": "node",
+            "sourceMap": true,
+            "emitDecoratorMetadata": true,
+            "experimentalDecorators": true,
+            "removeComments": false,
+            "noImplicitAny": false
         }))
         .pipe(gulp.dest('app'));
 });
---
+++ NodejsWebAppApplication/scripts/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'the-app',
+    template: '<h1>The Angular 2 App works like a charm!</h1>'
+})
+export class AppComponent { }
