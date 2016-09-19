ecbb1f19f8448b844834d011ece92deed2cee17e
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2016-09-19 11:09:42 +0200

    Node application added, copy from the repo https://github.com/Fjeddo/Node-TypeScript-Continuous-Delivery-To-Azure

---
+++ NodejsWebAppApplication/server.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var app = express();
+var port = process.env.port || 1337;
+
+app.use(express.static(__dirname));
+
+var server = app.listen(port, function() {
+        console.log('Minimal node express up and running on port ' + port);
+});
