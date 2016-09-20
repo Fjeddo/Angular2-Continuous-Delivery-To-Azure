fdd9fbea1a40c8c58823121b6a2452c634d07513
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2016-09-20 11:45:57 +0200

    Update app.component.ts

--- NodejsWebAppApplication/scripts/app.component.ts
+++ NodejsWebAppApplication/scripts/app.component.ts
@@ -1,9 +1,10 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'the-app',
     template: `
         <h1><a style="color: #555; text-decoration: none;" routerLink="/">The Angular 2 App works like a charm!</a></h1>
+        <p>There is one more route in the example: <a routerLink="/hello/Fjeddo">http://.../hello/[ENTER A NAME HERE]</a>, showing optional route parameter.</p>
         <nav>
               <a routerLink="/home">Home</a>
               <a routerLink="/about">About</a>
